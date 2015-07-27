105ec4159eec6a5ba454a602a939125cbc1289a5
fzerorubigd <fzerorubigd@gmail.com>
2015-07-27 13:58:28 +0430

    pass tests after update assertify

--- sink_test.go
+++ sink_test.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -27,15 +28,15 @@ func TestChain(t *testing.T) {
 
 	chain := NewChain(f1, f2)
 
-	assert.Equal(t, chain.functions[0], f1)
-	assert.Equal(t, chain.functions[1], f2)
+	assert.Equal(t, reflect.ValueOf(chain.functions[0]), reflect.ValueOf(f1))
+	assert.Equal(t, reflect.ValueOf(chain.functions[1]), reflect.ValueOf(f2))
 
 	assert.Len(t, chain.functions, 2)
 	cpy := chain.Extend(f3)
 
-	assert.Equal(t, cpy.functions[0], f1)
-	assert.Equal(t, cpy.functions[1], f2)
-	assert.Equal(t, cpy.functions[2], f3)
+	assert.Equal(t, reflect.ValueOf(cpy.functions[0]), reflect.ValueOf(f1))
+	assert.Equal(t, reflect.ValueOf(cpy.functions[1]), reflect.ValueOf(f2))
+	assert.Equal(t, reflect.ValueOf(cpy.functions[2]), reflect.ValueOf(f3))
 	assert.Len(t, chain.functions, 2)
 	assert.Len(t, cpy.functions, 3)
 }
